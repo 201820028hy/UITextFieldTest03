8b223a9120c74de1298d7efca4543eaa5abb1f49
dit03 <dit@imac-4.local>
2019-04-04 12:46:48 +0900

    TestTextField

--- UITextFieldTest03/ViewController.swift
+++ UITextFieldTest03/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myTextField.delegate = self//커넥션
+        myTextField.clearButtonMode = UITextField.ViewMode.always//
     }
 
-
+    @IBAction func btnPressed(_ sender: Any) {
+        myLabel.text = myTextField.text
+        myTextField.text = ""
+        myTextField.resignFirstResponder()//버튼 누르면 키패드 내려감
+    }
+    
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        myTextField.resignFirstResponder()//return버튼 누르면 키패드 내려감
+        myTextField.backgroundColor = UIColor.gray
+        return true
+    }
+    
+    public func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        view.backgroundColor = UIColor.black
+        return true
+    }
+    
 }
 
